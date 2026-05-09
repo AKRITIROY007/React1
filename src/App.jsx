1a09c9fafc944f738c02804a406883ed44141910
Akriti Roy <akritiroy8700@gmail.com>
2026-05-09 18:16:24 +0530

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,6 @@
+import { Footer } from "./Footer";
+import Homeee from "./Homeee";
+
 const Hello = () => {
   return <h2>THIS IS FIRST react COMPONENT</h2>;
 };
@@ -9,9 +12,12 @@ const Bye = () => {
 const App = () => {
   return (
     <div>
+      <Homeee />
+      
       <Bye />
       <Hello />
       <Hello />
+      <Footer/>
     </div>
   );
 };
