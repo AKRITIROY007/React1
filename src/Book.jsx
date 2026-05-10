e3c180b977707fa8bdc221eaa67cbde364a1d067
Akriti Roy <akritiroy8700@gmail.com>
2026-05-10 18:28:11 +0530

    Add book obj and obj destructutre and spread operator

--- src/Book.jsx
+++ src/Book.jsx
@@ -2,11 +2,13 @@ import React from "react";
 
 const Book = (props) => {
   console.log(props);
+
+  const { author, price, title } = props;
   return (
     <>
-      <h1>{props.writer}</h1>
-      <h2>{props.price}</h2>
-      <h1>{props.BookName}</h1>
+      <h1>{author}</h1>
+      <h1>{price}</h1>
+      <h1>{title}</h1>
     </>
   );
 };
--- src/BookList.jsx
+++ src/BookList.jsx
@@ -2,18 +2,22 @@ import React from "react";
 import Book from "./Book";
 
 const BookList = () => {
-  const author1 = "RAM";
-  const author2 = "YASH";
-  const title1 = "LETS LEARN C++";
-  const title2 = "LETS LEARN REACT ";
-  const price = 234;
+  const book1 = {
+    author: "yash",
+    title: "c",
+    price: "250",
+  };
+
+  const book2 = {
+    author: "Akriti",
+    title: "React",
+    price: "300",
+  };
 
   return (
     <>
-      <Book writer="Yashwant" BookName={title1} />
-      <Book writer={author2} BookName={title2} />
-      <Book writer={author1} BookName="lets learn HINDI" price={price} />
-      <Book writer={author1} BookName={title1} price="345" />
+      <Book {...book1} />
+      <Book {...book2} />
     </>
   );
 };
